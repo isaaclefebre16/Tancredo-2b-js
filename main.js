e6f760984b04cfd9a01b4d3b437e3b952928b23e
isaaclefebre16 <135345043+isaaclefebre16@users.noreply.github.com>
2023-09-18 10:36:21 -0300

    Aula 18-09-2023

--- main.js
+++ main.js
@@ -4,9 +4,8 @@ function tocaSom (idElementoAudio) {
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
 
-while (contador < listaDeTeclas.length){
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTeclas[contador];
 
@@ -20,8 +19,15 @@ while (contador < listaDeTeclas.length){
         tocaSom(idAudio);
     }
 
-    contador = contador + 1;
+    tecla.onkeydown = function (evento) {
+        if (evento.code == 'Space' || evento.code === 'Enter'){
+              tecla.classList.add('ativa');
+        }
+      
+    }
 
-    console.log(contador);
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
 
 }
